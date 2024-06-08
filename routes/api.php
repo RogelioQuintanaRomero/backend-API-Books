d4f621be6e5fe7191d988e82d61cb087e1cf5bd4
Rogelio Quintana Romero <oilegor_rqr@hotmail.com>
2024-06-07 19:30:13 -0600

    primera prueba de direcciones desde route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/* route API Book */
+// Index
+Route::get('/books', function(){
+    $data =[
+        'message' => 'Function /books desde route',
+        'status' => 200
+    ];
+    return response()->json($data,200);
+});
+
+// show
+Route::get('/books/{id}', function (){
+    return "Function GET /books/{id} desde route";
+});
+
+// store
+Route::post('/books', function(){ return " Function Post desde route";});
+// update
+Route::patch('/books/{id}', function(){ return " Function Patch books/{id} desde route";});
+// delete
+Route::delete('/books/{id}}', function(){ return " Function Delete books/{id} desde route";});
